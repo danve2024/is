bcc6122101e1db2ceee4a59ef642f4763be4176f
Jake Sully <iv.utool@gmail.com>
2024-06-03 02:22:57 +0400

    header bg, feedback everywhere

--- digest.js
+++ digest.js
@@ -7,49 +7,30 @@ function change() {
     i.style.display = "none";
   }
 
-  if (text === 'IT') {
-    var a = document.getElementsByClassName('IT');
-  } else if (text === "Экономика") {
-    var a = document.getElementsByClassName('economics');
-  } else if (text === "Маркетинг") {
-    var a = document.getElementsByClassName('marketing');
-  } else if (text === "Биология") {
-    var a = document.getElementsByClassName('biology');
-  } else if (text === "Медицина") {
-    var a = document.getElementsByClassName('medicine');
-  } else if (text === "Химия") {
-    var a = document.getElementsByClassName('chemistry');
-  } else if (text === "Физика") {
-    var a = document.getElementsByClassName('physics');
-  } else if (text === "Проектный менеджмент") {
-    var a = document.getElementsByClassName('leadership');
-  } else if (text === "PR") {
-    var a = document.getElementsByClassName('PR');
-  } else if (text === "Право") {
-    var a = document.getElementsByClassName('rights');
-  } else if (text === "Лингвистика") {
-    var a = document.getElementsByClassName('linguistics');
-  } else if (text === "История") {
-    var a = document.getElementsByClassName('history');
-  } else if (text === "Литература") {
-    var a = document.getElementsByClassName('literature');
-  } else if (text === "Социология") {
-    var a = document.getElementsByClassName('sociology');
-  } else if (text === "Журналистика") {
-    var a = document.getElementsByClassName('journalism');
-  } else if (text === "Техника") {
-    var a = document.getElementsByClassName('tech');
-  } else if (text === "Направление") {
-    var a = document.getElementsByClassName('content-card');
-  } else if (text === "Science") {
-    var a = document.getElementsByClassName('science');
-  } else if (text === "Соц-Гум") {
-    var a = document.getElementsByClassName('hum');
-  } else if (text === "Soft Skills") {
-    var a = document.getElementsByClassName('soft');
-  }
+  var classes = {
+    'IT': 'IT',
+    'Экономика': 'economics',
+    'Маркетинг': 'marketing',
+    'Биология': 'biology',
+    'Медицина': 'medicine',
+    'Химия': 'chemistry',
+    'Физика': 'physics',
+    'Проектный менеджмент': 'leadership',
+    'PR': 'PR',
+    'Право': 'rights',
+    'Лингвистика': 'linguistics',
+    'История': 'history',
+    'Литература': 'literature',
+    'Социология': 'sociology',
+    'Журналистика': 'journalism',
+    'Техника': 'tech',
+    'Направление': 'content-card',
+    'Science': 'science',
+    'Соц-Гум': 'hum',
+    'Soft Skills': 'soft'
+  };
   
-  
+  var a = document.getElementsByClassName(classes[text]);
 
   for (let i of a) {
     i.style.display = "flex";
