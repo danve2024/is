72863e87c92eea6081c797623552e5ff0098e8a2
Jake Sully <iv.utool@gmail.com>
2024-06-03 04:35:55 +0400

    progress-bar fix

--- scrollbar.js
+++ scrollbar.js
@@ -1,6 +1,6 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {updateProgressBar()};
 
-function myFunction() {
+function updateProgressBar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
