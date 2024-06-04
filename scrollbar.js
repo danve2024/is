583a078e5d90a4a863051ab8a1700d7ed997fb24
Jake Sully <iv.utool@gmail.com>
2024-06-05 02:06:37 +0400

    pscrollbar bugfix attempt

--- scrollbar.js
+++ scrollbar.js
@@ -1,15 +1,16 @@
-window.onscroll = function() {updateProgressBar()};
+document.addEventListener("DOMContentLoaded", function() {
+  updateProgressBar();
+  window.onscroll = function() {
+      updateProgressBar();
+  };
+});
 
 function updateProgressBar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
 
-  if (height <= 0) {
-    scrolled = 100; // Если нет прокрутки, прогресс-бар заполнен на 100%
-  }
+  if (height <= 0) { scrolled = 100; }
 
   document.getElementById("myBar").style.width = scrolled + "%";
 }
-
-document.addEventListener("DOMContentLoaded", updateProgressBar);
