a1b41026e7d1c21ab781bc3e37045487fab594d4
Dany <james.blackwood.jr@gmail.com>
2024-06-05 17:06:40 +0300

    dark mode footer improvement

--- script.js
+++ script.js
@@ -13,11 +13,15 @@ function toggleMenu() {
 function changeTheme() {
   const body = document.getElementById('body');
   const computedStyle = getComputedStyle(body);
+  const footer = document.getElementById('footer-p');
+  const link = document.getElementById('footer-link');
   const currentState = computedStyle.backgroundImage;
 
   if (currentState.includes('bg.jpg')) {
       body.style.backgroundImage = 'url("images/bg_dark.jpg")';
       body.style.color = 'white';
+      footer.style.color = 'white';
+      link.style.color = 'white';
       
   } else {
       body.style.backgroundImage = 'url("images/bg.jpg")';
