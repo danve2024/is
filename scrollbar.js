687e7f50f33051acf85771cf10e66ff2073d814a
Jake Sully <iv.utool@gmail.com>
2024-06-05 04:21:44 +0400

    more debug info

--- scrollbar.js
+++ scrollbar.js
@@ -5,14 +5,17 @@ function updateProgressBar() {
     console.log("myBar not found");
     return;
   }
-  
+  console.log("myBar found");
+
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
 
   if (height <= 0) { scrolled = 100; }
 
+  console.log("Scrolled:", scrolled, "%");
   document.getElementById("myBar").style.width = scrolled + "%";
+  myBar.style.width = scrolled + "%";
 }
 
 document.addEventListener("DOMContentLoaded", function() {
